66ccca1b4e57e84ee8893e4877711a077f7818c8
Khalteck <khalidoyeneye@gmail.com>
2022-10-07 16:51:25 -0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            this is the footer
+        </footer>
+    );
+}
+ 
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <main>
+            This is the main
+        </main>
+    );
+}
+ 
+export default Main;
