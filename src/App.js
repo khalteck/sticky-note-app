0e5bd07d8907cdaa779ac3851911748e186642b4
Khalteck <khalidoyeneye@gmail.com>
2022-10-08 16:22:48 -0600

    issue fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import Header from "./Header";
 import Main from "./Main";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Notes from "./Notes";
+import { useState } from "react"
+
 
 function App() {
+
+  //to set the default notes in state
+  const[note, setNote] = useState([
+    {
+        title: "Steps to take on how to look, smile, code and live like Khalid",
+        date: "may 15, 2022",
+        body: "Blah blah.. make your own note :)",
+        hover: false
+    },
+    {
+        title: "12 eye catching mobile wallpapers",
+        date: "june 16, 2022",
+        body: "Blah blah.. make your own note :)",
+        hover: false
+    },
+    {
+        title: "How to make your brand stand out online",
+        date: "sept 2, 2022",
+        body: "Blah blah.. make your own note :)",
+        hover: false
+    },
+    {
+        title: "How to code like Khalid",
+        date: "may 15, 2022",
+        body: "Blah blah.. make your own note :)",
+        hover: false
+    },
+    {
+        title: "10 great React coding practices",
+        date: "may 15, 2022",
+        body: "Blah blah.. make your own note :)",
+        hover: false
+    },
+])
+
+//to set hover state of each sticky note
+function handleNoteHover(index) {
+  const newNote = [...note];
+  newNote[index].hover = true;
+  setNote(newNote)
+}
+function handleNoteOut(index) {
+  const newNote = [...note];
+  newNote[index].hover = false;
+  setNote(newNote)
+}
+
   return (
    <Router>
     <div className="app">
@@ -18,7 +67,11 @@ function App() {
 
           {/*the notes page route */}
           <Route path="/notes">
-            <Notes/>
+            <Notes
+              note={note}
+              handleNoteHover={handleNoteHover}
+              handleNoteOut={handleNoteOut}
+            />
           </Route>
           {/*the notes page route */}
         </Switch>
--- src/Notes.js
+++ src/Notes.js
@@ -1,59 +1,26 @@
 //import { useState } from "react";
 
-const Notes = () => {
+const Notes = (props) => {
 
-    /*const[note, setNote] = useState([
-        {
-            title: "Step to take on how to look, smile, code and live like Khalid",
-            date: "may 15, 2022",
-            body: "Blah blah.. make your own note :)"
-        },
-        {
-            title: "12 eye catching mobile wallpapers",
-            date: "june 16, 2022",
-            body: "Blah blah.. make your own note :)"
-        },
-        {
-            title: "How to make your brand stand out online",
-            date: "sept 2, 2022",
-            body: "Blah blah.. make your own note :)"
-        },
-        {
-            title: "How to code like Khalid",
-            date: "may 15, 2022",
-            body: "Blah blah.. make your own note :)"
-        },
-        {
-            title: "10 great React coding practices",
-            date: "may 15, 2022",
-            body: "Blah blah.. make your own note :)"
-        },
-    ])*/
+    const eachNote = props.note?.map((item, index) => {
+        return (
+            <div 
+                onMouseOver={() => props.handleNoteHover(index)}
+                onMouseOut={() => props.handleNoteOut(index)}
+                className="p-[24px] bg-rose-400 cursor-pointer even:bg-[#ffab91] first:bg-[#e7ed9b] last:bg-[#cf94da] first:row-span-2 rounded-lg relative"
+            >
+                <div className={`w-[18px] h-[18px] rounded-full absolute top-2 ${item.hover ? "right-[47%]" : "right-2"} bg-[#252525] transition-all duration-500`}></div>
+                <h2 className="text-[1.5rem] font-[700] mb-[10px]">{item.title}</h2>
+                <h3>{item.date}</h3>
+            </div>
+        )
+    })
 
     return (
         <div className="w-full px-[100px] pt-[100px]">
             <h1 className="text-[2.5rem] mb-[50px] font-[700] tracking-wider">Notes</h1>
             <div className="w-full grid grid-cols-4 gap-[20px] text-[#252525]">
-                <div className="p-[20px] bg-rose-400 even:bg-[#ffab91] first:bg-[#e7ed9b] last:bg-[#cf94da] first:row-span-2 rounded-lg">
-                    <h2 className="text-[1.5rem] font-[700]">This is the Note title of this particular note</h2>
-                    <h3>Date created</h3>
-                </div>
-                <div className="p-[20px] bg-rose-400 even:bg-[#ffab91] first:bg-[#e7ed9b] last:bg-[#cf94da] first:row-span-2 rounded-lg">
-                    <h2 className="text-[1.5rem] font-[700]">This is the Note title of this particular note</h2>
-                    <h3>Date created</h3>
-                </div>
-                <div className="p-[20px] bg-rose-400 even:bg-[#ffab91] first:bg-[#e7ed9b] last:bg-[#cf94da] first:row-span-2 rounded-lg">
-                    <h2 className="text-[1.5rem] font-[700]">This is the Note title of this particular note</h2>
-                    <h3>Date created</h3>
-                </div>
-                <div className="p-[20px] bg-rose-400 even:bg-[#ffab91] first:bg-[#e7ed9b] last:bg-[#cf94da] first:row-span-2 rounded-lg">
-                    <h2 className="text-[1.5rem] font-[700]">This is the Note title of this particular note</h2>
-                    <h3>Date created</h3>
-                </div>
-                <div className="p-[20px] bg-rose-400 even:bg-[#ffab91] first:bg-[#e7ed9b] last:bg-[#cf94da] first:row-span-2 rounded-lg">
-                    <h2 className="text-[1.5rem] font-[700]">This is the Note title of this particular note</h2>
-                    <h3>Date created</h3>
-                </div>
+                {eachNote}
             </div>
         </div>
     );
