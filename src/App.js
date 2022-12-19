fbbb1afe770987e4f7c3f7103ba08e140d048c9e
khalteck <khalidoyeneye@gmail.com>
2022-12-19 18:27:28 +0100

    router6

--- src/App.js
+++ src/App.js
@@ -1,97 +1,43 @@
-import Header from "./Header";
 import Main from "./Main";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Notes from "./Notes";
-import { useState } from "react"
+import { useState } from "react";
 import Detail from "./Detail";
-import "./output.css"
-
+import "./output.css";
+import notedata from "./data/notedata.json";
 
 function App() {
-
   //to set the default notes in state
-  const[note, setNote] = useState([
-    {
-      id: 1,  
-      title: "Steps to take on how to look, smile, code and live like Khalid",
-      date: "may 15, 2022",
-      body: "Blah blah.. make your own note :) /n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-      hover: false
-    },
-    {
-      id: 2,  
-      title: "12 eye catching mobile wallpapers",
-      date: "june 16, 2022",
-      body: "Blah blah.. make your own note :)",
-      hover: false
-    },
-    {
-      id: 3,  
-      title: "How to make your brand stand out online",
-      date: "sept 2, 2022",
-      body: "Blah blah.. make your own note :)",
-      hover: false
-    },
-    {
-      id: 4,  
-      title: "How to code like Khalid",
-      date: "may 15, 2022",
-      body: "Blah blah.. make your own note :)",
-      hover: false
-    },
-    {
-      id: 5,  
-      title: "10 great React coding practices",
-      date: "may 15, 2022",
-      body: "Blah blah.. make your own note :)",
-      hover: false
-    },
-])
+  const [note, setNote] = useState(notedata);
 
-//to set hover state of each sticky note
-function handleNoteHover(index) {
-  const newNote = [...note];
-  newNote[index].hover = true;
-  setNote(newNote)
-}
-function handleNoteOut(index) {
-  const newNote = [...note];
-  newNote[index].hover = false;
-  setNote(newNote)
-}
+  //to set hover state of each sticky note
+  function handleNoteHover(index) {
+    const newNote = [...note];
+    newNote[index].hover = true;
+    setNote(newNote);
+  }
+  function handleNoteOut(index) {
+    const newNote = [...note];
+    newNote[index].hover = false;
+    setNote(newNote);
+  }
 
   return (
-   <Router>
-    <div className="app">
-      <Header/>
-      <div className="content">
-        <Switch>
-          {/*the homepage route */}
-          <Route exact path="/">
-            <Main/>
-          </Route>
-          {/*the homepage route */}
-
-          {/*the notes page route */}
-          <Route path="/notes">
-            <Notes
-              note={note}
-              handleNoteHover={handleNoteHover}
-              handleNoteOut={handleNoteOut}
-            />
-          </Route>
-          {/*the notes page route */}
-
-          <Route path="/note/:id">
-            <Detail
-              note={note}
-            />
-          </Route>
-        </Switch>
-      </div>
-    </div>
-   </Router>
-  )
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route
+        path="/notes"
+        element={
+          <Notes
+            note={note}
+            handleNoteHover={handleNoteHover}
+            handleNoteOut={handleNoteOut}
+          />
+        }
+      />
+      <Route path="/note/:id" element={<Detail note={note} />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/Notes.js
+++ src/Notes.js
@@ -1,33 +1,43 @@
 import { Link } from "react-router-dom";
+import Header from "./Header";
 
 const Notes = (props) => {
-
-    const eachNote = props.note?.map((item, index) => {
-        return (
-            <div 
+  return (
+    <>
+      <Header />
+      <div className="w-full px-[100px] pt-[100px]">
+        <h1 className="text-[2.5rem] mb-[50px] font-[700] tracking-wider">
+          Notes
+        </h1>
+        <div className="w-full grid grid-cols-4 gap-[20px] text-[#252525]">
+          {props.note?.map((item, index) => {
+            return (
+              <div
                 key={item.id}
                 onMouseOver={() => props.handleNoteHover(index)}
                 onMouseOut={() => props.handleNoteOut(index)}
                 className="p-[24px] bg-rose-400 cursor-pointer even:bg-[#ffab91] first:bg-[#e7ed9b] last:bg-[#cf94da] first:row-span-2 rounded-lg relative"
-            >
+              >
                 <Link to={`/note/${item.id}`}>
-                    <div className="overlay w-full h-full absolute top-0 left-0"></div>
+                  <div className="overlay w-full h-full absolute top-0 left-0"></div>
                 </Link>
-                <div className={`w-[18px] h-[18px] rounded-full absolute top-2 ${item.hover ? "right-[47%]" : "right-2"} bg-[#252525] transition-all duration-500`}></div>
-                <h2 className="text-[1.5rem] font-[700] mb-[10px]">{item.title}</h2>
+                <div
+                  className={`w-[18px] h-[18px] rounded-full absolute top-2 ${
+                    item.hover ? "right-[47%]" : "right-2"
+                  } bg-[#252525] transition-all duration-500`}
+                ></div>
+                <h2 className="text-[1.5rem] font-[700] mb-[10px]">
+                  {item.title}
+                </h2>
                 <h3>{item.date}</h3>
-            </div>
-        )
-    })
-
-    return (
-        <div className="w-full px-[100px] pt-[100px]">
-            <h1 className="text-[2.5rem] mb-[50px] font-[700] tracking-wider">Notes</h1>
-            <div className="w-full grid grid-cols-4 gap-[20px] text-[#252525]">
-                {eachNote}
-            </div>
+              </div>
+            );
+          })}
+          {/* {eachNote} */}
         </div>
-    );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default Notes;
